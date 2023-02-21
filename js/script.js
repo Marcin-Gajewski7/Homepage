e5dd9029a32671166cf88f2d16865c86ed8c5a73
Marcin Gajewski <marcingajewski159@gmail.com>
2023-02-21 01:42:22 +0000

    Functions  added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
-console.log("Hello World");
+const welcome = () => {
+    console.log("Hello World !!!");
+};
 
-let buttonElement = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
-let nameTheme = document.querySelector(".js-buttonName")
-
-buttonElement.addEventListener("click", () => {
+const onChangeBackgroundColor = () => {
+    const body = document.querySelector(".js-body");
+    const nameTheme = document.querySelector(".js-buttonName");
     body.classList.toggle("darkTheme");
     nameTheme.innerText = body.classList.contains("darkTheme") ? "jasny" : "ciemny";
-});
+};
+
+const init = () => {
+    const button = document.querySelector(".js-button");
+    button.addEventListener("click", onChangeBackgroundColor);
+}
+
+{
+    init();
+}
